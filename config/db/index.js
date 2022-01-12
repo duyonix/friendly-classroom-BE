75192a8f8723eaf9d45ee47758f478bf9377f41b
ydam <ydamk15nbk@gmail.com>
2022-01-12 08:37:03 +0700

    suceess deploy

--- config/db/index.js
+++ config/db/index.js
@@ -1,23 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const { GridFsStorage } = require('multer-gridfs-storage');
 
 async function connect() {
-    /*try {
-        const connection = await mongoose.connect(process.env.DATABASE_URL);
-        const storage = new GridFsStorage({ db: connection })
-        console.log('Connect successfully!!!');
-        return storage
-    } catch (error) {
-        console.log(error)
-        console.log('Connect failure!!!');
-    }*/
 
     try {
         await mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            // useCreateIndex: true,
+            useCreateIndex: true,
             // useFindAndModify: false,
         });
         console.log('Connect MongoDB successfully!!!');
